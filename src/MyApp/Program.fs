f21babc8f35fa99a4389417ada34987a2b8f3f7f
wk <somnuk.wk@gmail.com>
2020-11-05 18:02:35 +0700

    CM

--- src/MyApp/Program.fs
+++ src/MyApp/Program.fs
@@ -1,13 +1,28 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+[<Measure>]
+type g
+
+[<Measure>]
+type kg
+
+[<Measure>]
+type cm
+
+[<Measure>]
+type m
+
+[<Measure>]
+type inch
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let cmPerMeter: float<cm/m> = 100.0<cm/m>
+    let cmPerInc: float<cm/inch> = 2.54<cm/inch>
+    let convertCentimetersToInces (x: float<cm>) = x / cmPerInc
+
+    let l1 = 100.0<cm>
+    let l2 = convertCentimetersToInces l1
+    printfn "%A" l2
+    0
+
